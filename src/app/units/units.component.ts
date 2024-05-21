d253c9cc37b8434bf17ce64959313d41db887848
Dimitrije Glisic <dimitrije.glisic.ext@riag-digital.com>
2024-05-21 12:28:26 +0200

    improve 'units' looks

--- src/app/units/units.component.ts
+++ src/app/units/units.component.ts
@@ -1,21 +1,30 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {MatChip, MatChipSet} from "@angular/material/chips";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-units',
   standalone: true,
   imports: [
     MatChip,
-    MatChipSet
+    MatChipSet,
+    NgClass,
   ],
   templateUrl: './units.component.html',
   styleUrl: './units.component.css'
 })
 export class UnitsComponent {
 
+  _currentUnit: 'metric' | 'imperial' = 'metric';
+  @Input() set currentUnit(value: 'metric' | 'imperial') {
+    console.log('currentUnit', value);
+    this._currentUnit = value;
+  }
+
   @Output() unitChanged: EventEmitter<'metric' | 'imperial'> = new EventEmitter<'metric' | 'imperial'>();
 
   onUnitChange(metric: 'metric' | 'imperial') {
+    this.currentUnit = metric;
     this.unitChanged.emit(metric);
   }
 }
