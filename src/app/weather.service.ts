257e433aaeeb277932c8cfc486ac952e795b2b7d
Dimitrije Glisic <dimitrije.glisic.ext@riag-digital.com>
2024-05-24 15:29:07 +0200

    setup GithubActions CI

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {WeatherData} from "./model/weather-data";
-import {environment} from "../environment/environment";
+import {environment} from "../environments/environment";
 
 @Injectable({
   providedIn: 'root'
