cc43637ce03d94ff6218b61b60df693c797bef7a
Dimitrije Glisic <dimitrije.glisic.ext@riag-digital.com>
2024-05-27 16:37:07 +0200

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
-// Rename this file to `environment.ts` or `environment.prod.ts`
-// and fill in the actual value for apiKey.
 export const environment = {
+  production: true,
   cityApiUrl: 'http://api.openweathermap.org/geo/1.0/direct?q=',
   weatherApiUrl: 'https://api.openweathermap.org/data/3.0/onecall',
-  apiKey: 'your-openweathermap-api-key-here',
+  apiKey: '', // This will be set in the CI/CD pipeline
 }
