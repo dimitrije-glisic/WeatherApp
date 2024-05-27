1a3edc1d7a3da08afcfd5d7fdb24d4b0f140f76f
dimitrije-glisic <dimitrije-glisic@users.noreply.github.com>
2024-05-27 15:06:04 +0000

    deploy: 739e6fc30003cdffc3553f2c3d22b2a3a59367c1

--- src/environments/environment-example.ts
+++
@@ -1,7 +0,0 @@
-// Rename this file to `environment.ts` or `environment.prod.ts`
-// and fill in the actual value for apiKey.
-export const environment = {
-  cityApiUrl: 'http://api.openweathermap.org/geo/1.0/direct?q=',
-  weatherApiUrl: 'https://api.openweathermap.org/data/3.0/onecall',
-  apiKey: 'your-openweathermap-api-key-here',
-}
--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  cityApiUrl: 'http://api.openweathermap.org/geo/1.0/direct?q=',
-  weatherApiUrl: 'https://api.openweathermap.org/data/3.0/onecall',
-  apiKey: '', // This will be set in the CI/CD pipeline
-}
